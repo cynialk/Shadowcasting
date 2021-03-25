695d4467434b2d4540294d287a17f5726f46ce86
cynialk <53977410+cynialk@users.noreply.github.com>
2021-03-25 20:02:08 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Shadowcasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(200, 800, "cool");
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+                Raylib.DrawText("Yo what's up homie", 2, 5, 10, Color.BLACK);
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
